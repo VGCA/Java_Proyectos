8aa04ac687c0653bc717d9de6837ebf9787e2bd9
VGCA <bboygabri@gmail.com>
2025-07-25 13:08:14 +0100

    My main portfolio under construction

---
+++ Thymeleaf_Portfolio/src/main/java/com/portfolio/Thymeleaf_Portfolio/ThymeleafPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Thymeleaf_Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPortfolioApplication.class, args);
+	}
+
+}
